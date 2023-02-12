f56051d11f1c687e39b3a92dee15b17aaf6e512c
AngelKusik <angelica.kusik@dcmail.ca>
2023-02-11 22:42:04 -0500

    Projects page mainly finished - missing js code to change link on the Navbar

--- WEBD6201_lab1/Scripts/app.js
+++ WEBD6201_lab1/Scripts/app.js
@@ -45,7 +45,8 @@
 
         //set buttons href attribute & img src
         homeButton.href = "./about.html"
-        homeImg.src = "./Content/Images/Picture2.png"
+        //Ghost Image retrieved from: https://www.pngaaa.com/detail/115465
+        homeImg.src = "./Content/Images/GhostImageFromPngaaadetail115465.png"
         homeImg.alt = "Ghost Image"
 
         //Append elements to the left column
@@ -82,7 +83,7 @@
         //Declare string variables to hold text content for text elements
         let wwdTitle = "What We Do"
         let wwdText1 = "We are full-stack web developers ready to build the perfect solution for your company. Front-end, back-end, we can do it all."
-                    + "Get ready to take your business to the next level with fast, modern, responsive web applications that are sure to impress your clients."
+                    + " Get ready to take your business to the next level with fast, modern, responsive web applications that are sure to impress your clients."
         let wwdtext2 = "Click on the button below to check out some of our latest projects."
         let citeText = "Image Source:"
 
@@ -120,8 +121,149 @@
 
     }
 
+    function DisplayProjectsPage() {
+
+        /******* Projects Section *******/
+
+        let numberOfCards = 4
+
+        //Get the projects section col 
+        let projactsCol = document.getElementById("projects-main-col")
+
+        //Create col elements 
+        let projectsHeading1 = document.createElement("h1")
+        
+        let cardImg1 = document.createElement("img")
+        let cardImg2 = document.createElement("img")
+        let cardImg3 = document.createElement("img")
+        let cardImg4 = document.createElement("img")
+
+        let cardTitle1 = document.createElement("h5")
+        let cardTitle2 = document.createElement("h5")
+        let cardTitle3 = document.createElement("h5")
+        let cardTitle4 = document.createElement("h5")
+
+        let cardParagraph1 = document.createElement("p")
+        let cardParagraph2 = document.createElement("p")
+        let cardParagraph3 = document.createElement("p")
+        let cardParagraph4 = document.createElement("p")
 
 
+        //Add some attributes to elements to style them
+        projectsHeading1.setAttribute("class", "mt-3 pt-5 display-2 text-center")
+
+        cardImg1.setAttribute("class", "card-img-top card-image-size")
+        cardImg2.setAttribute("class", "card-img-top card-image-size")
+        cardImg3.setAttribute("class", "card-img-top card-image-size")
+        cardImg4.setAttribute("class", "card-img-top card-image-size")
+
+        cardTitle1.setAttribute("class", "card-title")
+        cardTitle2.setAttribute("class", "card-title")
+        cardTitle3.setAttribute("class", "card-title")
+        cardTitle4.setAttribute("class", "card-title")
+
+        cardParagraph1.setAttribute("class", "card-text")
+        cardParagraph2.setAttribute("class", "card-text")
+        cardParagraph3.setAttribute("class", "card-text")
+        cardParagraph4.setAttribute("class", "card-text")
+
+
+        //Declare string variables to hold the text value of text elements
+        let projectsTitle = "Our Latest Projects"
+        let card1Title = "Eisha's WEBD-3201 Project"
+        let card1Text = "This is a website we worked on for Web Development Intermediate (WEBD-3201) using PHP."
+        let card2Title = "Angelica's Tin Dog Project"
+        let card2Text = "This was a Summer Project implemented using Bootstrap."
+        let card3Title = "Card title"
+        let card3Text = "This card has supporting text below as a natural lead-in to additional content."
+        let card4Title = "Angelica's Writing for The Web Project"
+        let card4Text = "This was a Web Project Developed for Durham College Professional and part-Time Learning."
+
+        //Add text value to elements
+        projectsHeading1.textContent = projectsTitle
+        cardTitle1.textContent = card1Title
+        cardTitle2.textContent = card2Title
+        cardTitle3.textContent = card3Title
+        cardTitle4.textContent = card4Title
+        cardParagraph1.textContent = card1Text
+        cardParagraph2.textContent = card2Text
+        cardParagraph3.textContent = card3Text
+        cardParagraph4.textContent = card4Text
+
+        //set buttons href attribute & img src
+        cardImg1.src = "./Content/Images/EishaWebd3201.png"
+        cardImg2.src = "./Content/Images/AngelicaTinDogProject.jpg"
+        cardImg3.src = "./Content/Images/MicrosoftTeams-image.png"
+        cardImg4.src = "./Content/Images/AngelicaWritingForTheWebProject.jpg"
+        cardImg1.alt = "Eisha's Webd 3201 Project Dashboard Page"
+        cardImg2.alt = "Angelica's TinDog Summer Project"
+        cardImg3.alt = "Technologies Image"
+        cardImg4.alt = "Angelica's Writing for The Web Project"
+
+        //Get cards and add image
+        let card1 = document.getElementById("card1")
+        card1.prepend(cardImg1)
+
+        let card2 = document.getElementById("card2")
+        card2.prepend(cardImg2)
+
+        let card3 = document.getElementById("card3")
+        card3.prepend(cardImg3)
+
+        let card4 = document.getElementById("card4")
+        card4.prepend(cardImg4)
+
+        //Get card body and add text elements
+        let card1Body = document.getElementsByClassName("card-body")[0]
+        card1Body.appendChild(cardTitle1)
+        card1Body.appendChild(cardParagraph1)
+
+        let card2Body = document.getElementsByClassName("card-body")[1]
+        card2Body.appendChild(cardTitle2)
+        card2Body.appendChild(cardParagraph2)
+
+        let card3Body = document.getElementsByClassName("card-body")[2]
+        card3Body.appendChild(cardTitle3)
+        card3Body.appendChild(cardParagraph3)
+
+        let card4Body = document.getElementsByClassName("card-body")[3]
+        card4Body.appendChild(cardTitle4)
+        card4Body.appendChild(cardParagraph4)
+
+
+        //Add elements to the page
+        projactsCol.prepend(projectsHeading1)
+
+
+        //Lastly add footer to page
+        DisplayFooter()
+    }
+
+    function DisplayAboutUsPage() {
+
+        //Lastly add footer to page
+        DisplayFooter()
+    }
+
+    function DisplayServicesPage() {
+
+
+        //Lastly add footer to page
+        DisplayFooter()
+    }
+
+    function DisplayContactUsPage() {
+
+
+        //Lastly add footer to page
+        DisplayFooter()
+    }
+
+    function DisplayHumanResourcesPage() {
+
+        //Lastly add footer to page
+        DisplayFooter()
+    }
 
 
     /**** Footer ******/
@@ -155,7 +297,6 @@
 
     }
 
-
     
     /**** On Load ******/
     function Start() {
@@ -164,26 +305,23 @@
           case "Lab 1 - Home Page":
             DisplayHomePage()
             break;
-          case "About - WEBD6201 Demo":
-            DisplayAbout()
+          case "Lab 1 - Projects Page":
+            DisplayProjectsPage()
             break;
-          case "Contact List - WEBD6201 Demo":
-            DisplayContactList()
+          case "Lab 1 - About Us Page":
+            DisplayAboutUsPage()
             break;
-          case "Contact Us - WEBD6201 Demo":
-            DisplayContacts()
+          case "Lab 1 - Services Page":
+            DisplayServicesPage()
             break;
-          case "Projects - WEBD6201 Demo":
-            DisplayProjects()
+          case "Lab 1 - Contact Us Page":
+            DisplayContactUsPage()
             break;
-          case "References - WEBD6201 Demo":
-            DisplayReferences()
-            break;
-          case "Services - WEBD6201 Demo":
-            DisplayServices()
+          case "Lab 1 - Human Resources Page":
+            DisplayHumanResourcesPage()
             break;
           default:
-          // code block
+          console.log("Something went wrong in the Start Function")
         }
       }
 
