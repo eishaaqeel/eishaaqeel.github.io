3afd9eadf4ff9ddb8fb705757b1f2642f7ebcf6c
Eisha Aqeel <eisha.aqeel@dcmail.ca>
2023-03-29 19:50:06 -0400

    ICE11

--- WEBD6201_ICE11/server.js
+++ WEBD6201_ICE11/server.js
@@ -1,9 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const http = require('http');
+const http_1 = __importDefault(require("http"));
 const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
+const port = 3010;
+const server = http_1.default.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
